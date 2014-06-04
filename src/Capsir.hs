aced78a7195ba9be1f3a5a7597e74c675b6b4b53
Brian Chin <naerbnic@ucla.edu>
2014-06-04 14:46:13 -0700

    First import

---
+++ src/Capsir.hs
@@ -0,0 +1,66 @@
+module Capsir where
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (fromJust)
+
+data Cont = Cont
+    ![String]  -- Formal parameter names
+    !CpsExpr   -- The body expression of the continuation
+
+data Const = ConstFunc   -- A const creating function
+               !String   -- The name of the function
+               ![Const]  -- The arguments to the function
+           | ConstInt !Int -- A constant int
+           | ConstString !String  -- A constant string
+           | ConstFloat !Double  -- A constant float
+
+data Value = ContValue !Cont  -- A literal continuation value
+           | VarValue !String  -- A variable reference value
+           | ConstValue !Const  -- A constant data value
+
+data CpsExpr = Apply ![Value]  -- The arguments to apply to the continuation
+                     !Value    -- A value that evaluates to a continuation
+             | Inst !String   -- The instruction name
+                    ![Value]  -- The instruction parameters
+                    ![Value]  -- | A list continuation values that the inst can 
+                              -- call.
+             | Fix ![(String, Cont)]  -- A mapping from function names to conts
+                   !CpsExpr           -- The expression to execute in this environment
+             | Exit  -- Exits the program
+
+-- Runtime semantic information
+
+data ContInst = ContInst Env Cont
+data RuntimeValue = InstRuntimeValue ContInst
+                  | ConstRuntimeValue Const
+
+data Env = EmptyEnv
+         | FrameEnv (Map String RuntimeValue) Env
+
+
+lookupEnv :: String -> Env ->  Maybe RuntimeValue
+lookupEnv _ EmptyEnv = Nothing
+lookupEnv name (FrameEnv envMap child) =
+    case Map.lookup name envMap of
+      Just val -> Just val
+      Nothing -> lookupEnv name child
+
+
+eval :: Value -> Env -> RuntimeValue
+eval (ContValue cont) env = InstRuntimeValue (ContInst env cont)
+eval (VarValue name) env = fromJust (lookupEnv name env)
+eval (ConstValue const) _ = ConstRuntimeValue const
+
+evalAsCont :: Value -> Env -> ContInst
+evalAsCont val env = case eval val env of
+    InstRuntimeValue inst -> inst
+    _ -> error "Expected a continuation; Got something else"
+
+data ExecState = ExecState Env CpsExpr
+
+step :: ExecState -> Maybe ExecState
+step (ExecState _ Exit) = Nothing
+step (ExecState env (Apply args val)) =
+    let cont = evalAsCont val env
+    in undefined
