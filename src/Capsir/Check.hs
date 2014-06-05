41e97a316348e7230390c353bebe8c4de4197655
Brian Chin <naerbnic@ucla.edu>
2014-06-05 02:09:27 -0700

    Minor changes.

---
+++ src/Capsir/Check.hs
@@ -0,0 +1,10 @@
+module Capsir.Check where
+
+import Capsir
+-- import Data.Map (Map)
+-- import qualified Data.Map as Map
+
+data Type = ContType [Type]
+          | LitType 
+
+check :: CpsExpr -> Bool
